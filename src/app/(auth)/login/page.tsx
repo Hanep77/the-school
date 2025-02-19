342b4ac1451dfffcd210f5f632f1748ac255e93b
hanep77 <yudishan26@gmail.com>
2025-02-19 16:48:53 +0700

    modified input component and login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -41,15 +41,14 @@ export default function Login() {
     <div className="min-h-screen flex flex-col justify-center items-center">
       <div className="p-5 shadow bg-white rounded w-80">
         <h4 className="text-2xl text-center font-semibold mb-5">Sign in</h4>
-        <form onSubmit={authRequest} className="flex flex-col gap-2">
+        <form onSubmit={authRequest} className="flex flex-col gap-3">
           <Input type="email" name="email" label="Email" required={true} />
           <Input type="password" name="password" label="Password" required={true} />
-          <label htmlFor="role">Pilih role:</label>
-          <select name="cars" id="cars" className="h-8 border border-zinc-300 rounded px-2">
+          <select name="cars" id="cars" className="h-8 border border-zinc-400 rounded px-2">
             <option value="admin">Admin</option>
             <option value="guru">Guru</option>
           </select>
-          <button type="submit" className="h-8 bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white rounded">
+          <button type="submit" className="h-10 bg-cyan-700 hover:bg-cyan-600 active:bg-cyan-500 text-white rounded">
             Sign in
           </button>
         </form>
--- src/app/components/input.tsx
+++ src/app/components/input.tsx
@@ -1,4 +1,8 @@
-interface InputType {
+"use client"
+
+import { useState } from "react"
+
+interface InputProps {
   type: string,
   name: string,
   label: string,
@@ -7,13 +11,24 @@ interface InputType {
   disabled?: boolean
 }
 
-export default function Input({ type, name, label, error, required, disabled }: InputType) {
-  return (
-    <div className="flex flex-col">
-      {error && <p>{error}</p>}
-      <label htmlFor={name}>{label}</label>
-      <input id={name} type={type} name={name} disabled={disabled} required={required}
-        className="h-8 border border-zinc-300 rounded px-2" />
-    </div>
-  )
+export default function Input({ type, name, label, error, required, disabled }: InputProps) {
+  const [value, setValue] = useState<string | null>(null)
+
+  return <div className="relative w-full border border-zinc-400 rounded pt-2">
+    {error && <p>{error}</p>}
+    <input
+      type={type}
+      id={name}
+      name={name}
+      className="w-full peer outline-none h-8 px-4 rounded"
+      onKeyUp={(e) => setValue((e.target as HTMLInputElement).value)}
+      required={required}
+      disabled={disabled}
+    />
+    <label
+      htmlFor={name}
+      className={`peer-focus:text-sm ${value && "-top-2 text-sm px-2"} peer-focus:-top-2 left-2 absolute transition-all cursor-text bg-white peer-focus:px-2 text-zinc-800`}>
+      {label}
+    </label>
+  </div>
 }
